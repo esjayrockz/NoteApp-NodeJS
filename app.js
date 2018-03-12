5002e247ad3bfb0f8e03c0283179113258ac3607
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-03-12 02:07:02 -0400

    Final commit to Note App

--- app.js
+++ app.js
@@ -1,23 +1,68 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
-//var res = notes.addNote();
-var sum = notes.add(10.2, -2);
-console.log(sum);
-// var user = os.userInfo();
-//console.log(user);
-// fs.appendFile('greetings.txt',' Hello '+ user.username+'! Your age is '+ notes.age + '.', err => {
-// if(err){
-//   console.log('Unable to write file');
-// }
-//
-// });
-//
-// fs.appendFile('greetings.txt',` Your uid is ${os.userInfo().username}!`, err => {
-// if(err){
-//   console.log('Unable to write file');
-// }
-//
-// });
+
+const titleOptions = {
+  describe:'Title of note',
+  demand: true,
+  alias: 't'
+};
+const bodyOptions = {
+  describe:'Body of note',
+  demand: true,
+  alias: 'b'
+};
+
+const argv = yargs
+.command('add','Add a new note',{
+  title: titleOptions,
+  body: bodyOptions
+})
+.command('list','List all notes')
+.command('read','Read a note',{
+  title: titleOptions
+})
+.command('remove','Remove a note',{
+  title: titleOptions
+})
+.help()
+.argv;
+var command = argv._[0];
+
+if(command === 'add'){
+  var note = notes.addNote(argv.title, argv.body);
+  if(note){
+    console.log('Note added');
+    notes.logNote(note);
+  }
+  else{
+    console.log('Note not saved. Note with this title already present');
+  }
+}
+else if(command === 'list')
+{
+  var allNotes = notes.getAll();
+  console.log(`Listing ${allNotes.length} note(s).`);
+  allNotes.forEach((note)=> notes.logNote(note));
+
+}
+else if(command === 'read'){
+  var note = notes.getNote(argv.title);
+  if(note){
+    console.log('Note found');
+    notes.logNote(note);
+  }
+  else{
+    console.log('Note not found');
+  }
+}
+else if(command === 'remove'){
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note was removed' : 'Note not found';
+  console.log(message);
+}
+else{
+  console.log('Not a valid command for the note app');
+}
